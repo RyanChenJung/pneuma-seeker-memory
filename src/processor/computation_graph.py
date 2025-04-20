7a43107daea1e73bb9415d3192f3659fd2512077
luthfibalaka <luthfibalaka@gmail.com>
2025-04-20 09:22:35 +0700

    Improve computation graph

--- src/processor/computation_graph.py
+++ src/processor/computation_graph.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from typing import Any
+from inspect import currentframe
+from typing import Any, Optional
 from uuid import uuid4
 
 
@@ -8,13 +9,17 @@ class Node:
         self,
         computation_description: str,
         computation_output: Any,
-        input_nodes: list["Node"] = None,
+        function_name: str,
+        class_name: Optional[str],
+        input_nodes: Optional[list["Node"]] = None,
     ):
         self.id = str(uuid4())
         self.timestamp = str(datetime.now())
         self.input_nodes = input_nodes or []
         self.computation_description = computation_description
         self.computation_output = computation_output
+        self.function_name = function_name
+        self.class_name = class_name
 
     def to_dict(self) -> dict[str, Any]:
         return {
@@ -23,6 +28,8 @@ class Node:
             "input_nodes": [input_node.id for input_node in self.input_nodes],
             "computation_description": self.computation_description,
             "computation_output": self.computation_output,
+            "function_name": self.function_name,
+            "class_name": self.class_name,
         }
 
 
@@ -30,8 +37,29 @@ class ComputationGraph:
     def __init__(self):
         self.nodes: list[Node] = []
 
-    def add_node(self, node: Node):
-        self.nodes.append(node)
+    def create_node(
+        self,
+        computation_description: str,
+        computation_output: Any,
+        input_nodes: Optional[list["Node"]] = None,
+    ):
+        # Grab the caller info using inspect
+        frame = currentframe()
+        caller_frame = frame.f_back
+        function_name = caller_frame.f_code.co_name
+        class_name = None
+        if "self" in caller_frame.f_locals:
+            class_name = type(caller_frame.f_locals["self"]).__name__
+
+        self.nodes.append(
+            Node(
+                computation_description,
+                computation_output,
+                function_name,
+                class_name,
+                input_nodes,
+            )
+        )
 
     def to_json(self):
         return {"nodes": [node.to_dict() for node in self.nodes]}
