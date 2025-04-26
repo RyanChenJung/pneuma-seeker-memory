ff78c4b9094d2658452e234e6e6175d85c05647d
luthfibalaka <luthfibalaka@gmail.com>
2025-04-26 03:08:14 -0500

    Add simple string cleaner

--- src/processor/utils/string_processor.py
+++ src/processor/utils/string_processor.py
@@ -1,6 +1,20 @@
 from ast import literal_eval
 
 
+def clean_code_string(code: str):
+    if code.startswith("```"):
+        code = code[3:]
+    if code.endswith("```"):
+        code = code[:-3]
+    if code.startswith("python"):
+        code = code[6:]
+    elif code.startswith("sql"):
+        code = code[3:]
+    elif code.startswith("json"):
+        code = code[4:]
+    return code
+
+
 def parse_code_string(code: str):
     """
     Parses strings that represent code (SQL script, Python list, etc.)
@@ -17,11 +31,12 @@ def parse_code_string(code: str):
         code = code[4:]
     return literal_eval(code)
 
+
 def parse_sql_string(code: str):
     if code.startswith("```"):
         code = code[3:]
     if code.endswith("```"):
         code = code[:-3]
-    elif code.startswith("sql"):
+    if code.startswith("sql"):
         code = code[3:]
     return code
