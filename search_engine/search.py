ce9eb1c04ac4d942fac1d0293c447470e4758722
luthfibalaka <luthfibalaka@gmail.com>
2025-05-06 03:41:52 -0500

    Update search engine interface

--- search_engine/search.py
+++ search_engine/search.py
@@ -1,23 +1,23 @@
 import bm25s
 import Stemmer
 
-from tqdm import tqdm
-
-hitrates_data: list[dict[str, str]] = []
-
 
 def search(
     question: str,
     k: int,
     index_path: str,
-):
+) -> list[str]:
     retriever = bm25s.BM25.load(index_path, load_corpus=True)
     stemmer = Stemmer.Stemmer("english")
 
     query_tokens = bm25s.tokenize(question, stemmer=stemmer, show_progress=False)
     results, _ = retriever.retrieve(query_tokens, k=k, show_progress=False)
+
+    search_results: list[str] = []
     for result_idx, result in enumerate(results[0]):
         print(f"=> Result {result_idx}: {result['text']}")
+        search_results.append(result['text'])
+    return search_results
 
 
 if __name__ == "__main__":
