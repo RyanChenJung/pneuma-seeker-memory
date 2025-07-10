6b796d1b6879db7c8d722c0ff151d472d996aad9
luthfibalaka <luthfibalaka@gmail.com>
2025-07-10 14:42:00 -0500

    Convert downloader to script

---
+++ src/processor/model/weight/model_downloader.py
@@ -0,0 +1,12 @@
+from huggingface_hub import hf_hub_download, snapshot_download
+from huggingface_hub import login
+
+# Embedding model
+repo_id = "BAAI/bge-base-en-v1.5"
+local_dir = "bge-base"
+snapshot_download(repo_id, local_dir=local_dir, resume_download=True)
+
+# LLM
+repo_id = "Qwen/Qwen2.5-7B-Instruct"
+local_dir = "qwen25-7b"
+snapshot_download(repo_id, local_dir=local_dir, resume_download=True)
