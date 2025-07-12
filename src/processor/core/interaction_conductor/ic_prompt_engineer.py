24ed939bfffc023674532a1096ad08347d53fad6
luthfibalaka <luthfibalaka@gmail.com>
2025-07-12 14:19:00 -0500

    Introduce prompt engineer

---
+++ src/processor/core/interaction_conductor/ic_prompt_engineer.py
@@ -0,0 +1,15 @@
+class ICPromptEngineer:
+    def get_general_prompt():
+        """
+        Returns prompt for general chat.
+        """
+
+    def get_ir_prompt():
+        """
+        Returns prompt for the IR system.
+        """
+    
+    def get_materializer_prompt():
+        """
+        Returns prompt for the materializer engine.
+        """
