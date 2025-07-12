1657802614cd2befffdb893be7adab2fd9d62527
luthfibalaka <luthfibalaka@gmail.com>
2025-07-12 15:06:41 -0500

    Implement early prompt for sanity checking IR system's output

--- src/processor/core/interaction_conductor/ic_prompt_engineer.py
+++ src/processor/core/interaction_conductor/ic_prompt_engineer.py
@@ -8,7 +8,10 @@ class ICPromptEngineer:
         """
         Returns prompt for the IR system.
         """
-    
+
+    def get_ir_sanity_check_prompt(self, prompt: str, results: str):
+        return f"Given this prompt: `{prompt}`, does these results from an IR system make sense: `{results}`? If not, provide some feedback. Begin your answer with yes/no, followed by the feedback if the result doesn't make sense."
+
     def get_materializer_prompt():
         """
         Returns prompt for the materializer engine.
