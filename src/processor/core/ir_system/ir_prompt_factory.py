fe02ea7cfd019e3538289c9c049bdfb75fd9c407
luthfibalaka <luthfibalaka@gmail.com>
2025-07-17 15:49:30 -0500

    Improve prompt for retrieval results refinement

--- src/processor/core/ir_system/ir_prompt_factory.py
+++ src/processor/core/ir_system/ir_prompt_factory.py
@@ -2,8 +2,8 @@ class IRPromptFactory:
     def get_new_retrieve_prompt(self, requirements: str):
         return f"""Given these requirements: `{requirements}`, please craft a prompt to be given to information retrieval systems."""
 
-    def get_refine_retrieval_prompt(self, prev_prompt: str, feedback: str):
-        return f"""This is the previous prompt given to some information retrieval systems: `{prev_prompt}`, which is imperfect. This is the user feedback: `{feedback}`. Please adjust the prompt by incorporating the feedback to increase the change of getting more relevant results. Answer directly."""
+    def get_refine_retrieval_prompt(self, prev_prompt: str, prev_docs_repr: str, feedback: str):
+        return f"""This is the previous prompt given to an information retrieval system: `{prev_prompt}`, which is imperfect, as it results in the following documents being retrieved: `{prev_docs_repr}`. This is the user feedback: `{feedback}`. Please adjust the prompt by incorporating the feedback to increase the chance of getting more relevant results. Answer directly."""
 
     def get_retriever_classification_prompt(
         self, prompt: str, retriever_name: str, retriever_desc: str
