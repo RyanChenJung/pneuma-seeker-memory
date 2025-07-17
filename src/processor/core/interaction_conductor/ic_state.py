6233c173583d7e22c89ff1a81618009932e9b78a
luthfibalaka <luthfibalaka@gmail.com>
2025-07-17 15:48:33 -0500

    Remove unnecessary state method

--- src/processor/core/interaction_conductor/ic_state.py
+++ src/processor/core/interaction_conductor/ic_state.py
@@ -7,23 +7,16 @@ class ICState:
         Initializes the session state with empty SQLs and target schemas.
         """
         self.sqls: list[str] = []
-        self.target_schemas: list[DataFrame] = []
-    
-    def set_state(self, new_sqls: list[str], new_target_schemas: list[DataFrame]) -> None:
+        self.target_schemas: dict[str, DataFrame] = dict()
+
+    def set_state(self, new_sqls: list[str], new_target_schemas: dict[str, DataFrame]) -> None:
         """
         Sets new values for SQLs and Target Schemas
         """
         self.sqls = new_sqls
         self.target_schemas = new_target_schemas
 
-    def reset(self) -> None:
-        """
-        Sets the values of both SQLs and Target Schemas to be empty.
-        """
-        self.sqls = []
-        self.target_schemas = []
-
-    def get_state(self) -> dict[str, list[str | DataFrame]]:
+    def get_state(self) -> dict[str, list[str] | dict[str, DataFrame]]:
         """
         Returns the current SQLs and Target Schemas.
         """
