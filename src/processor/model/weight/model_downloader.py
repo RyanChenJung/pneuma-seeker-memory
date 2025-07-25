14740363780c7c84969c22da1ee1a072b0de2eda
luthfibalaka <luthfibalaka@gmail.com>
2025-07-25 09:15:41 -0500

    Migrate to Qwen 3 8B

--- src/processor/model/weight/model_downloader.py
+++ src/processor/model/weight/model_downloader.py
@@ -7,10 +7,11 @@ local_dir = "bge-base"
 snapshot_download(repo_id, local_dir=local_dir, resume_download=True)
 
 # LLM
-repo_id = "Qwen/Qwen2.5-7B-Instruct"
-local_dir = "qwen25-7b"
+repo_id = "Qwen/Qwen3-8B"
+local_dir = "qwen3-8b"
 snapshot_download(repo_id, local_dir=local_dir, resume_download=True)
 
-repo_id = "Qwen/Qwen3-4B"
-local_dir = "qwen25-7b"
-snapshot_download(repo_id, local_dir=local_dir, resume_download=True)
+# OLD MODEL:
+# repo_id = "Qwen/Qwen2.5-7B-Instruct"
+# local_dir = "qwen25-7b"
+# snapshot_download(repo_id, local_dir=local_dir, resume_download=True)
