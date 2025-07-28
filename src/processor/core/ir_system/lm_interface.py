17c85ec13e853ce193c038930d1f551fc9886388
luthfibalaka <luthfibalaka@gmail.com>
2025-07-28 08:34:42 -0500

    Disable sanity checker

--- src/processor/core/ir_system/lm_interface.py
+++ src/processor/core/ir_system/lm_interface.py
@@ -112,92 +112,95 @@ class LMInterface:
                 self.logger.info(
                     f"==> {retrieved_document}"
                 )
+            self.logger.info("=" * 50)
+            
+            all_retrieval_results[retriever_type] = curr_retrieval_results
 
-            relevant_retrieval_results: set[AbstractDocument] = set()
-            irrelevant_doc_ids: list[str] = []
-            total_sanity_check_iteration = 0
-            while (
-                curr_retrieval_results
-                and len(relevant_retrieval_results) < k
-                and total_sanity_check_iteration < MAX_SANITY_CHECK_ITERATIONS
-            ):
-                self.logger.info(
-                    f"=> Starting sanity check iteration {total_sanity_check_iteration + 1}"
-                )
-                sanity_check_messages = [
-                    LLMMessage(
-                        role=Role.USER.value,
-                        content=self.prompt_factory.get_ir_sanity_check_prompt(
-                            prompt,
-                            convert_retrieval_results_to_str(curr_retrieval_results),
-                        ),
-                    )
-                ]
+            # relevant_retrieval_results: set[AbstractDocument] = set()
+            # irrelevant_doc_ids: list[str] = []
+            # total_sanity_check_iteration = 0
+            # while (
+            #     curr_retrieval_results
+            #     and len(relevant_retrieval_results) < k
+            #     and total_sanity_check_iteration < MAX_SANITY_CHECK_ITERATIONS
+            # ):
+            #     self.logger.info(
+            #         f"=> Starting sanity check iteration {total_sanity_check_iteration + 1}"
+            #     )
+            #     sanity_check_messages = [
+            #         LLMMessage(
+            #             role=Role.USER.value,
+            #             content=self.prompt_factory.get_ir_sanity_check_prompt(
+            #                 prompt,
+            #                 convert_retrieval_results_to_str(curr_retrieval_results),
+            #             ),
+            #         )
+            #     ]
 
-                sanity_check_result: IRFeedbackOutputType = parse_json(
-                    self.llm.chat(sanity_check_messages, LLMOption(json_mode=True))
-                )
-                total_sanity_check_iteration += 1
+            #     sanity_check_result: IRFeedbackOutputType = parse_json(
+            #         self.llm.chat(sanity_check_messages, LLMOption(json_mode=True))
+            #     )
+            #     total_sanity_check_iteration += 1
 
-                irrelevant_doc_ids = sanity_check_result["irrelevant_doc_ids"]
-                feedback = sanity_check_result["feedback"]
-                self.logger.info(
-                    f"=> Found {len(irrelevant_doc_ids)} irrelevant documents: {irrelevant_doc_ids}"
-                )
-                self.logger.info(
-                    f"=> Feedback: {feedback}"
-                )
+            #     irrelevant_doc_ids = sanity_check_result["irrelevant_doc_ids"]
+            #     feedback = sanity_check_result["feedback"]
+            #     self.logger.info(
+            #         f"=> Found {len(irrelevant_doc_ids)} irrelevant documents: {irrelevant_doc_ids}"
+            #     )
+            #     self.logger.info(
+            #         f"=> Feedback: {feedback}"
+            #     )
 
-                for i in curr_retrieval_results:
-                    if i.doc_id not in irrelevant_doc_ids:
-                        relevant_retrieval_results.add(i)
+            #     for i in curr_retrieval_results:
+            #         if i.doc_id not in irrelevant_doc_ids:
+            #             relevant_retrieval_results.add(i)
                 
-                if len(irrelevant_doc_ids) == 0:
-                    self.logger.info(
-                        f"==> Since there are no irrelevant documents, we don't need to do another sanity check."
-                    )
-                    break
-                elif len(irrelevant_doc_ids) > 0 and feedback != "":
-                    self.logger.info(f"==> Re-retrieving documents with feedback: {feedback}")
-                    curr_retrieval_results = self.re_retrieve_with_feedback(
-                        retriever_type,
-                        feedback,
-                        [
-                            i
-                            for i in curr_retrieval_results
-                            if i.doc_id in irrelevant_doc_ids
-                        ],
-                        k,
-                        sources,
-                    )
-                    self.logger.info(
-                        f"=> Adjusted retrieval returned {len(curr_retrieval_results)} documents:"
-                    )
-                    for retrieved_document in curr_retrieval_results:
-                        self.logger.info(
-                            f"==> {retrieved_document}"
-                        )
+            #     if len(irrelevant_doc_ids) == 0:
+            #         self.logger.info(
+            #             f"==> Since there are no irrelevant documents, we don't need to do another sanity check."
+            #         )
+            #         break
+            #     elif len(irrelevant_doc_ids) > 0 and feedback != "":
+            #         self.logger.info(f"==> Re-retrieving documents with feedback: {feedback}")
+            #         curr_retrieval_results = self.re_retrieve_with_feedback(
+            #             retriever_type,
+            #             feedback,
+            #             [
+            #                 i
+            #                 for i in curr_retrieval_results
+            #                 if i.doc_id in irrelevant_doc_ids
+            #             ],
+            #             k,
+            #             sources,
+            #         )
+            #         self.logger.info(
+            #             f"=> Adjusted retrieval returned {len(curr_retrieval_results)} documents:"
+            #         )
+            #         for retrieved_document in curr_retrieval_results:
+            #             self.logger.info(
+            #                 f"==> {retrieved_document}"
+            #             )
 
-            # If at the end, we have less than k results, we should not discard the supposedly
-            # irrelevant results. Let's keep them.
-            irrelevant_retrieval_results = [
-                i for i in curr_retrieval_results if i.doc_id in irrelevant_doc_ids
-            ]
-            idx = 0
-            while len(relevant_retrieval_results) < k and idx < len(
-                irrelevant_retrieval_results
-            ):
-                relevant_retrieval_results.add(irrelevant_retrieval_results[idx])
-                idx += 1
+            # # If at the end, we have less than k results, we should not discard the supposedly
+            # # irrelevant results. Let's keep them.
+            # irrelevant_retrieval_results = [
+            #     i for i in curr_retrieval_results if i.doc_id in irrelevant_doc_ids
+            # ]
+            # idx = 0
+            # while len(relevant_retrieval_results) < k and idx < len(
+            #     irrelevant_retrieval_results
+            # ):
+            #     relevant_retrieval_results.add(irrelevant_retrieval_results[idx])
+            #     idx += 1
 
-            all_retrieval_results[retriever_type] = list(relevant_retrieval_results)
-            self.logger.info(
-                f"Final results for {retriever_type}: {len(relevant_retrieval_results)} documents:"
-            )
-            for retrieved_document in curr_retrieval_results:
-                self.logger.info(
-                    f"==> {retrieved_document}"
-                )
+            # all_retrieval_results[retriever_type] = list(relevant_retrieval_results)
+            # self.logger.info(
+            #     f"Final results for {retriever_type}: {len(relevant_retrieval_results)} documents:"
+            # )
+            # for retrieved_document in curr_retrieval_results:
+            #     self.logger.info(
+            #         f"==> {retrieved_document}"
+            #     )
 
         self.logger.info("Document retrieval completed")
         return all_retrieval_results
