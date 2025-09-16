6cb195a83269c6e09f1cba7d6c7d77c32f9c0fe7
luthfibalaka <luthfibalaka@gmail.com>
2025-09-16 08:17:10 -0500

    Fix external data bug

--- src/pneuma_seeker/core/conductor/main.py
+++ src/pneuma_seeker/core/conductor/main.py
@@ -374,6 +374,7 @@ class Conductor:
                 self.info_need_state.column_descriptions,
                 self.info_need_state.sqls,
                 note,
+                self.external_documents,
             )
             self.info_need_state.is_target_schemas_materialized = True
             return "Successfully materialized the target schemas."
