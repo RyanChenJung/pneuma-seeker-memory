0dde31129a52d7116e6c1190cdc4e75059ec75b5
luthfibalaka <luthfibalaka@gmail.com>
2025-09-19 14:15:59 -0500

    Improve UI/UX of state view

--- src/pneuma_seeker/server.py
+++ src/pneuma_seeker/server.py
@@ -147,15 +147,6 @@ async def read_combined_html(request: Request, user_id: str, chat_id: str):
     conductor = manager.get_chat_interface(user_id, chat_id).llm_conductor
     state = conductor.info_need_state.get_current_state_instance()
     prov_graph_html = conductor.prov_graph.get_graph_visualization()
-
-    match = re.search(
-        r"<body.*?>(.*?)</body>", prov_graph_html, re.IGNORECASE | re.DOTALL
-    )
-    if match:
-        prov_graph_html = match.group(1)
-    else:
-        prov_graph_html = prov_graph_html
-
     return templates.TemplateResponse(
         "index2.html",
         {"request": request, "state": state, "prov_graph_html": prov_graph_html},
