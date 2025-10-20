0d1cfd3b383505fa5fc4a21b8918d2e945e1febc
luthfibalaka <luthfibalaka@gmail.com>
2025-10-20 13:18:19 -0500

    Add web search as a hyperparameter

--- src/pneuma_seeker/utils/config.py
+++ src/pneuma_seeker/utils/config.py
@@ -16,10 +16,15 @@ class Config:
             "OPENWEBUI_BASE_URL", "http://localhost:8080/"
         )
         self.OPENWEBUI_API_KEY = os.getenv("OPENWEBUI_API_KEY", "")
+
         self.CONDUCTOR_ITERATION_LIMIT = int(
             os.getenv("CONDUCTOR_ITERATION_LIMIT", "5")
         )
+
         self.SEMANTIC_JOIN_TOP_K = 1
+
+        self.ENABLE_WEB_SEARCH = False
+
 
     def __get_use_azure(self):
         use_azure = os.getenv("USE_AZURE", "false").lower()
